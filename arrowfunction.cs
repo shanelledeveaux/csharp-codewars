7040a91adb31a19982fb6831e6677d8c6942a691
shanellewebster <webstershanelle@gmail.com>
2019-01-29 20:53:56 -0600

    arrow functions

---
+++ arrowfunction.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+  public static List<int> Odds(List<int> values) =>
+    values.Where(x => x % 2 == 1)
+          .ToList();
+}
